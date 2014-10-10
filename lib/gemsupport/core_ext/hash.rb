5ff25fe0aa40d55f3a1153ac52b6a551db87139f
mdouchement <marc.douchement@predicsis.com>
2014-10-10 16:31:27 +0200

    Add Hash#deep_merge Hash#deep_merge! and keys from activesupport

---
+++ lib/gemsupport/core_ext/hash.rb
@@ -0,0 +1,15 @@
+Hash.class_eval do
+  def deep_merge(hsh)
+    merger = proc do |key, v1, v2|
+      v1.kind_of?(Hash) && v2.kind_of?(Hash) ? v1.merge(v2, &merger) : v2
+    end
+    self.merge(hsh, &merger)
+  end
+
+  def deep_merge!(hsh)
+    merger = proc do |key, v1, v2|
+      v1.kind_of?(Hash) && v2.kind_of?(Hash) ? v1.merge(v2, &merger) : v2
+    end
+    self.merge!(hsh, &merger)
+  end
+end
---
+++ spec/gemsupport/core_ext/hash_keys_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Hash do
+  let(:simple) do
+    {
+      strings: { 'a' => 1, 'b' => 2 },
+      upcase_strings: { 'A' => 1, 'B' => 2 },
+      symbols: { a: 1, b: 2 },
+      mixed: { 'a' => 1, b: 2 },
+      fixnums: { 0 => 1, 1 => 2 }
+    }
+  end
+
+  let(:nested) do
+    {
+      strings: { 'a' => { 'b' => { 'c' => 3 } } },
+      upcase_strings: { 'A' => { 'B' => { 'C' => 3 } } },
+      symbols: { a: { b: { c: 3 } } },
+      mixed: { 'a' => { b: { 'c' => 3 } } },
+      fixnums: { 0 => { 1 => { 'c' => 3 } } }
+    }
+  end
+
+  describe '#transform_keys' do
+    # to long to write and already tested from activesupport
+  end
+end
