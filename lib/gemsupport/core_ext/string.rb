ebeea4773b8252bcec8d9f89c542cd0ed88cc394
mdouchement <marc.douchement@predicsis.com>
2015-01-09 13:03:31 +0100

    Add String#unindent feature

--- lib/gemsupport/core_ext/string.rb
+++ lib/gemsupport/core_ext/string.rb
@@ -13,4 +13,19 @@ String.class_eval do
       e.split('_').map { |w| w.capitalize }.join
     end.join('::')
   end
+
+  # Remove indentation spaces for multilines string
+  # From:
+  # |     Anonymous Coward
+  # |       - Community Guest
+  # To:
+  # |Anonymous Coward
+  # |  - Community Guest
+  def unindent
+    gsub(/^#{scan(/^[ \t]+(?=\S)/).min}/, '')
+  end
+
+  def unindent!
+    replace(unindent)
+  end
 end
