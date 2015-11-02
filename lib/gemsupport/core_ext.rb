06c172dae594b2e3e33416d8cdd88a6890f129ee
mdouchement <marc.douchement@predicsis.com>
2015-11-02 10:44:38 +0100

    Do not require some core_ext if ActiveSupport is defined

--- lib/gemsupport/core_ext.rb
+++ lib/gemsupport/core_ext.rb
@@ -1,5 +1,5 @@
-require 'gemsupport/core_ext/blank'
+require 'gemsupport/core_ext/blank' unless defined?(ActiveSupport)
 require 'gemsupport/core_ext/string'
-require 'gemsupport/core_ext/hash_keys'
+require 'gemsupport/core_ext/hash_keys' unless defined?(ActiveSupport)
 require 'gemsupport/core_ext/hash'
 require 'gemsupport/core_ext/object_deep_clone'
